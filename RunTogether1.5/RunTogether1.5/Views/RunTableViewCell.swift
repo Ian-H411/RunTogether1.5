94eaae78a30e67b9ed791dab8404c5a37a02b83b
Ian_H411 <ian.h411@gmail.com>
2019-09-16 08:43:03 -0600

    adds a few icons and slims down a considerable amount of UI

---
+++ RunTogether1.5/RunTogether1.5/Views/RunTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  RunTableViewCell.swift
+//  RunTogether1.5
+//
+//  Created by Ian Hall on 9/16/19.
+//  Copyright © 2019 Ian Hall. All rights reserved.
+//
+
+import UIKit
+
+class RunTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var playerVsPlayerLabel: UILabel!
+    
+    @IBOutlet weak var distanceLabel: UILabel!
+
+}
